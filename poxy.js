7cce654556f0b586fe2c3a8d3d96f091b060ad9f
Andrey web2025-5 <malyi.andrew@gmail.com>
2025-05-12 00:07:44 +0300

    Додано підтримку GET, PUT, DELETE з кешу

--- poxy.js
+++ poxy.js
@@ -2,6 +2,7 @@ const http = require('http');
 const fs = require('fs').promises;
 const path = require('path');
 const { program } = require('commander');
+const url = require('url');
 
 program
   .requiredOption('-h, --host <host>', 'Server host')
@@ -20,4 +21,35 @@ const server = http.createServer((req, res) => {
 
 server.listen(port, host, () => {
   console.log(`Server running at http://${host}:${port}`);
+});
+server.on('request', async (req, res) => {
+  const parsedUrl = url.parse(req.url, true);
+  const code = parsedUrl.pathname.replace('/', '');
+  const filePath = path.join(cache, `${code}.jpg`);
+
+  try {
+    if (req.method === 'GET') {
+      const data = await fs.readFile(filePath);
+      res.writeHead(200, { 'Content-Type': 'image/jpeg' });
+      res.end(data);
+    } else if (req.method === 'PUT') {
+      let body = [];
+      req.on('data', chunk => body.push(chunk));
+      req.on('end', async () => {
+        await fs.writeFile(filePath, Buffer.concat(body));
+        res.writeHead(201);
+        res.end('Created');
+      });
+    } else if (req.method === 'DELETE') {
+      await fs.unlink(filePath);
+      res.writeHead(200);
+      res.end('Deleted');
+    } else {
+      res.writeHead(405);
+      res.end('Method not allowed');
+    }
+  } catch (err) {
+    res.writeHead(404);
+    res.end('Not found');
+  }
 });
